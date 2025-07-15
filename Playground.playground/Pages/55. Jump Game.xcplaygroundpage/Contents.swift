c6b6a9c6618f6b6d95a0f6e57bb901a1f39a0f26
Anatol <yarmolovich1986@gmail.com>
2025-07-15 13:18:45 +0200

    [master]: 55. Jump Game

---
+++ Playground.playground/Pages/55. Jump Game.xcplaygroundpage/Contents.swift
@@ -0,0 +1,62 @@
+/*
+ You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+ Return true if you can reach the last index, or false otherwise.
+
+
+
+ Example 1:
+
+ Input: nums = [2,3,1,1,4]
+ Output: true
+ Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ Example 2:
+
+ Input: nums = [3,2,1,0,4]
+ Output: false
+ Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+
+ Constraints:
+
+ 1 <= nums.length <= 104
+ 0 <= nums[i] <= 105
+ */
+
+import Foundation
+
+class Solution55 {
+    func canJump(_ nums: [Int]) -> Bool {
+        var reach = 0
+        var i = 0
+        while i <= reach {
+
+            reach = max(reach, i + nums[i])
+            i += 1
+            if reach >= nums.count - 1 { return true }
+        }
+
+        return false
+    }
+}
+
+func firstTest()  {
+    let input = [2,3,1,1,4]
+    let result = Solution55().canJump(input)
+    assert(result == true, "test failed")
+    print("✅ \(#function) passed")
+}
+
+func secondTest()  {
+    let input = [3,2,1,0,4]
+    let result = Solution55().canJump(input)
+    assert(result == false, "test failed")
+    print("✅ \(#function) passed")
+}
+
+func runAllTests()  {
+    firstTest()
+    secondTest()
+}
+
+runAllTests()
